bb38ba5a59628389291efe87a98d6198d7c5f9fb
dmitrytelpov <dmitry.telpov@ifixgroup.com>
2023-09-12 15:48:16 +0400

    Network manager updated

--- MVVM+Boxing/OnlineStore(MVVM+Boxing)/NetworkLayer/NetworkManager.swift
+++ MVVM+Boxing/OnlineStore(MVVM+Boxing)/NetworkLayer/NetworkManager.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-/// Custom Error enum that we'll use in case
 enum NetworkError: Error {
     case noInternet
     case apiFailure
@@ -15,7 +14,6 @@ enum NetworkError: Error {
     case decodingError
 }
 
-/// An enum for various HTTPMethod. I've implemented GET and POST. I'll update the code and add the rest shortly :D
 fileprivate enum HTTPMethod: String {
     case get     = "GET"
     case post    = "POST"
@@ -24,23 +22,18 @@ fileprivate enum HTTPMethod: String {
     case delete  = "DELETE"
 }
 
-/// to provide data to the API Calls
 typealias Parameters = [String : Any]
 
-
-/// for encoding the Query Parameters in case of a GET call. Queries are passed in the ?q=<>&<> format
 fileprivate enum URLEncoding {
     case queryString
     case none
     
     func encode(_ request: inout URLRequest, with parameters: Parameters)  {
         switch self {
-        /// In case we need to pass Query Params to GET / Rarely for POST requests too
         case .queryString:
             guard let url = request.url else { return }
             if var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false),
                !parameters.isEmpty {
-                
                 urlComponents.queryItems = [URLQueryItem]()
                 for (k, v) in parameters {
                     let queryItem = URLQueryItem(name: k, value: "\(v)".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed))
@@ -48,8 +41,6 @@ fileprivate enum URLEncoding {
                 }
                 request.url = urlComponents.url
             }
-            
-        /// default case f
         case .none:
             break
         }
@@ -97,7 +88,6 @@ fileprivate enum APIRequestManager: APIRequestProtocol {
     fileprivate func asURLRequest() -> URLRequest {
         /// appends the path passed to either of the enum case with the base URL
         var request = URLRequest(url: Self.baseURL.appendingPathComponent(path))
-        
         /// appends the httpMethod based on the enum case
         request.httpMethod = method.rawValue
         
@@ -105,16 +95,10 @@ fileprivate enum APIRequestManager: APIRequestProtocol {
         
         switch self {
         case .getAPI(_, let queries):
-            /// we are just going through all the key and value pairs in the queries and adding the same to parameters.. For Each Key-Value pair,  parameters[key] = value
             queries.forEach({parameters[$0] = $1})
-            /// encode the queries for GET call //
             URLEncoding.queryString.encode(&request, with: parameters)
-            
         case .postAPI(_, let queries):
-            /// we are just going through all the key and value pairs in the queries and adding the same to parameters.. For Each Key-Value pair,  parameters[key] = value
             queries.forEach({parameters[$0] = $1})
-            
-            /// We serialise the Dictionary into a Data format so that it can be passed as a httpBody
             if let jsonData = try? JSONSerialization.data(withJSONObject: parameters) {
                 request.httpBody = jsonData
             }
@@ -123,7 +107,6 @@ fileprivate enum APIRequestManager: APIRequestProtocol {
         return request
     }
     
-    /// This function calls the URLRequest passed to it, maps the result and returns it. It is called by GET and POST.
     fileprivate static func makeRequest<S: Codable>(session: URLSession, request: URLRequest, model: S.Type, onCompletion: @escaping(S?, NetworkError?) -> ()) {
         session.dataTask(with: request) { data, response, error in
             guard error == nil, let responseData = data else { onCompletion(nil, NetworkError.apiFailure) ; return }
@@ -133,8 +116,6 @@ fileprivate enum APIRequestManager: APIRequestProtocol {
                     let jsonData = try JSONSerialization.data(withJSONObject: json)
                     let response = try JSONDecoder().decode(S.self, from: jsonData)
                     onCompletion(response, nil)
-                    
-                    /// if the response is an `Array of Objects`
                 } else if let json = try JSONSerialization.jsonObject(with: responseData, options: .mutableContainers)
                             as? [Parameters] {
                     let jsonData = try JSONSerialization.data(withJSONObject: json)
@@ -163,7 +144,6 @@ fileprivate enum APIRequestManager: APIRequestProtocol {
         } else {
             fullPath = path
         }
-        
         let request: URLRequest = Self.getAPI(path: fullPath, data: queries).asURLRequest()
         
         makeRequest(session: session, request: request, model: T.self) { (result, error) in
@@ -185,10 +165,7 @@ fileprivate enum APIRequestManager: APIRequestProtocol {
 
 /// Wrapper for Network Requests, has functions for various API Requests, and passes the queries to the Generic GET/POST functions.
 struct NetworkManager {
-    
     // MARK:- functions
-    
-    /// Fetches Array of Posts. Note that I've passed T as [Post]?
     func getProducts(category: String?, onCompletion: @escaping (Result<[Product]?, NetworkError>) -> ()) {
         APIRequestManager.makeGetRequest(path: "products", category: category, queries: [:]) { (result: [Product]?, error) in
             if let error = error {
@@ -196,16 +173,6 @@ struct NetworkManager {
             } else {
                 onCompletion(.success(result))
             }
-        }
-    }
-    
-    /// Puts a post using POST API. Here T passed is [String : String]?
-    func addPost(onCompletion: @escaping ([String: String]?, NetworkError?) -> ()) {
-        /// hardcoded body for now
-        let body: Parameters = [ "id": 101, "title": "Hello World", "body": "How you doin?", "userId": 1 ]
-    
-        APIRequestManager.makePostRequest(path: "/posts", body: body)  { ( result: [String: String]?, error) in
-                onCompletion(result, error)
         }
     }
 }
